b33925643d0441df5dc2cc998dadf8669692a1ba
Miguel dos Santos <miguedossantos@furg.br>
2021-04-26 17:38:38 -0300

    Pronto

--- App.js
+++ App.js
@@ -1,21 +1,243 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Image,
+  ImageBackground,
+  View,
+  TextInput,
+  Pressable,
+  Text,
+  Modal,
+  ActivityIndicator
+} from 'react-native';
 
-export default function App() {
+import titleImage from "./images/title.png";
+import backgroundImage from "./images/background.png";
+
+
+const App = () => {
+  const [CEP, setCEP] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [address, setAddress] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleCEPChange = (value) => {
+    if (value.length > 5) {
+      if (value.includes('-')) {
+        setCEP(`${value.slice(0, 5)}-${value.slice(6, 9)}`);
+      } else {
+        setCEP(`${value.slice(0, 5)}-${value.slice(5, 8)}`);
+      } 
+    } else {
+      setCEP(value);
+    }
+  }
+
+  const searchCEP = async () => {
+    setLoading(true);
+    await fetch(`https://viacep.com.br/ws/${CEP.slice(0, 5)}${CEP.slice(6, 9)}/json/`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.erro) {
+          setError("CEP não encontrado!");
+          setAddress(null);
+        } else {
+          setAddress(json);
+          setError(null);
+        }
+      })
+      .catch((error) => {
+        setAddress(null);
+        setError("Erro interno do sistema!");
+      });
+    setLoading(false);
+  }
+
+  const handleSearch = async () => {
+    setModalVisible(true);
+    searchCEP();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={backgroundImage} style={styles.background}>
+        <Image style={styles.titleImage}
+          source={titleImage}
+        />
+        <TextInput style={styles.input}
+          value={CEP}
+          placeholder="Digite o CEP"
+          onChangeText={handleCEPChange}
+        />
+        <Pressable
+          style={CEP.length === 9 ? styles.enabledButton : styles.disabledButton}
+          disabled={CEP.length === 9 ? false : true}
+          onPress={handleSearch}
+        >
+          <Text style={styles.buttonText}>{`Buscar`}</Text>
+        </Pressable>
+      </ImageBackground>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.modalView}>
+          <View style={styles.centeredView}>
+            {loading ? (
+              <ActivityIndicator size="large" color="ffdf2b" />
+            ) : (
+              error ? (
+                <>
+                  <Text style={styles.title}>Erro!</Text>
+                  <Text style={styles.text}>{error}</Text>
+                </>
+              ) : address ? (
+                <>
+                  <Text style={styles.title}>Informações</Text>
+                  <Text style={styles.text}>{`CEP: ${address.cep}`}</Text>
+                  <Text style={styles.text}>{`Logradouro: ${address.logradouro || 'Não informado'}`}</Text>
+                  <Text style={styles.text}>{`Complemento: ${address.complemento || 'Não informado'}`}</Text>
+                  <Text style={styles.text}>{`Bairro: ${address.bairro || 'Não informado'}`}</Text>
+                  <Text style={styles.text}>{`Localidade: ${address.localidade || 'Não informado'}`}</Text>
+                  <Text style={styles.text}>{`UF: ${address.uf || 'Não informado'}`}</Text>
+                </>
+              ) : (
+                <>
+                </>
+              )
+            )}
+            <Pressable
+              style={styles.modalButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalButtonText}>{`Fechar`}</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  background: {
+    flex: 1,
+    alignSelf: 'stretch',
+    resizeMode: "cover",
+    alignItems: 'center',
+    justifyContent: "center"
+  },
+
+  titleImage: {
+    width: 230,
+    height: 150,
+    marginBottom: 75
+  },
+
+  input: {
+    width: 275,
+    height: 50,
+    fontSize: 18,
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: "#59522a",
+    marginBottom: 75,
+    borderRadius: 25,
+    backgroundColor: "#fff",
+  },
+
+  enabledButton: {
+    width: 150,
+    height: 45,
+    borderRadius: 20,
+    backgroundColor: "#ffdf2b"
+  },
+
+  disabledButton: {
+    width: 150,
+    height: 45,
+    borderRadius: 20,
+    backgroundColor: "#ffdf2baa"
+  },
+
+  buttonText: {
+    color: "#59522a",
+    fontSize: 18,
+    lineHeight: 45,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  modalView: {
+    flex: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  
+  centeredView: {
+    width: 350,
+    height: 700,
+    marginTop: 22,
+    alignItems: "center",
+    borderColor: "#59522a",
+    borderWidth: 2,
+    borderRadius: 25,
+    justifyContent: "center",
+    backgroundColor: "#eaeaea",
+  },
+
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+
+  text: {
+    margin: 5,
+    fontSize: 16,
+    textAlign: 'center'
+  },
+
+  modalButton: {
+    width: 120,
+    height: 40,
+    marginTop: 50,
+    borderRadius: 15,
+    backgroundColor: "#ffdf2b"
+  },
+
+  modalButtonText: {
+    color: "#59522a",
+    fontSize: 16,
+    textAlign: 'center',
+    lineHeight: 38,
+    fontWeight: 'bold',
+  },
+
+  mapContainer: {
+    height: 300,
+    width: 300
+  },
+
+  map: {
+    flex: 1
+  }
 });
+
+
+export default App;
